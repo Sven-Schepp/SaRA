416829f55a30be608bb6c3c2459ae19613d27d9e
Sven <svenschepp@yahoo.de>
2022-02-14 14:24:16 +0100

    Updated GNU-licence notice.

--- ReachLib/include/occupancy.hpp
+++ ReachLib/include/occupancy.hpp
@@ -9,10 +9,7 @@ TUM, either version 3 of the License, or
 Reach-RI is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see https://www.gnu.org/licenses/.
+GNU General Public License for more details: https://www.gnu.org/licenses/.
 */
 
 #include <iostream>
