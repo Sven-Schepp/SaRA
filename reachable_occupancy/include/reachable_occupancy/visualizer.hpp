0251153d5be2d4092205db6156ba49a738b90c70
Sven <svenschepp@yahoo.de>
2022-06-16 14:36:36 +0200

    Updated license notice

--- reachable_occupancy/include/reachable_occupancy/visualizer.hpp
+++ reachable_occupancy/include/reachable_occupancy/visualizer.hpp
@@ -1,12 +1,12 @@
 /*
-This file is part of Reach-RI.
+This file is part of SaRA.
 
-Reach-RI is free software: you can redistribute it and/or modify
+SaRA is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 TUM, either version 3 of the License, or
 (at your option) any later version.
 
-Reach-RI is distributed in the hope that it will be useful,
+SaRA is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details: https://www.gnu.org/licenses/.
