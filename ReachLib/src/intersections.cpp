31ff4aa8fe6720b2e6387d51aaac0ba67e1a7727
JakobThumm <jakob.thumm@tum.de>
2023-11-06 11:45:05 -0800

    Moved min_segment_distance function to cpp file as it was missing an inline in the hpp.

--- ReachLib/src/intersections.cpp
+++ ReachLib/src/intersections.cpp
@@ -61,5 +61,82 @@ double segmentsDistance(const Capsule& c1, const Capsule& c2) {
     return Point::norm(closest_point_1, closest_point_2);
   }
 }
+
+double min_segment_distance(const Capsule& c1, const Capsule& c2) {
+  // Calculate denominator
+  Point A = c1.p2_ - c1.p1_;
+  Point B = c2.p2_ - c2.p1_;
+  double magA = Point::norm(A);
+  double magB = Point::norm(B);
+
+  Point _A(A.x/magA, A.y/magA, A.z/magA);
+  Point _B(B.x/magB, B.y/magB, B.z/magB);
+
+  Point cross = Point::cross(_A, _B);
+  double denom = std::pow(Point::norm(cross), 2);
+
+  // If Capsules are parrallel denom = 0
+  if (denom == 0) {
+    double d0 = Point::inner_dot(_A, c2.p1_ - c1.p1_);
+    double d1 = Point::inner_dot(_A, c2.p2_ - c1.p1_);
+
+    if (d0 <= 0 >= d1) {
+      if (std::abs(d0) < std::abs(d1)) {
+        return Point::norm(c1.p1_ - c2.p1_);
+      } else {
+        return Point::norm(c1.p1_ - c2.p2_);
+      }
+    } else if (d0 >= magA <= d1) {
+      if (std::abs(d0) < std::abs(d1)) {
+        return Point::norm(c1.p2_ - c2.p1_);
+      } else {
+        return Point::norm(c1.p2_ - c2.p2_);
+      }
+    } else {
+      return Point::norm(Point(_A.x*d0, _A.y*d0, _A.z*d0) + c1.p1_ - c2.p1_);
+    }
+  } else {
+    Point t = c2.p1_ - c1.p1_;
+    double detA = Point::determinant3x3(t, _B, cross); // Maybe switch A, B
+    double detB = Point::determinant3x3(t, _A, cross);
+
+    double t0 = detA/denom;
+    double t1 = detB/denom;
+
+    Point pA = c1.p1_ + Point(_A.x*t0, _A.y*t0, _A.z*t0);
+    Point pB = c2.p1_ + Point(_B.x*t1, _B.y*t1, _B.z*t1);
+
+    if (t0 < 0) {
+      pA = c1.p1_;
+    } else if (t0 > magA) {
+      pA = c1.p2_;
+    } else if (t1 < 0) {
+      pB = c2.p1_;
+    } else if (t1 > magB) {
+      pB = c2.p2_;
+    }
+
+    if (t0 < 0 || t0 > magA) {
+      double dot = Point::inner_dot(_B, pA - c2.p1_);
+      if (dot < 0) {
+        dot = 0;
+      } else if (dot > magB) {
+        dot = magB;
+      }
+      pB = c2.p1_ + Point(_B.x*dot, _B.y*dot, _B.z*dot);
+
+    }
+    if (t1 < 0 || t1 > magB) {
+      double dot = Point::inner_dot(_A, pB - c1.p1_);
+      if (dot < 0) {
+        dot = 0;
+      } else if (dot > magA) {
+        dot = magA;
+      }
+      pA = c1.p1_ + Point(_A.x*dot, _A.y*dot, _A.z*dot);
+    }
+    return Point::norm(pA - pB);
+  }
+}
 }  //  namespace intersections
 }  //  namespace occupancy_containers
